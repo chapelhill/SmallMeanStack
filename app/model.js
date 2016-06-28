3385669ff1c42ddc44ba4ae75ecf4f9089cb92b6
Ervin Wu <ecwu@live.unc.edu>
2016-06-28 15:09:29 -0400

    added server and database

---
+++ app/model.js
@@ -0,0 +1,24 @@
+// Pulls Mongoose dependency for creating schemas
+var mongoose    = require('mongoose');
+var Schema      = mongoose.Schema;
+
+// Creates a User Schema. This will be the basis of how user data is stored in the db
+var UserSchema = new Schema({
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
+   
+    created_at: {type: Date, default: Date.now},
+    updated_at: {type: Date, default: Date.now}
+});
+
+// Sets the created_at parameter equal to the current time
+UserSchema.pre('save', function(next){
+    now = new Date();
+    this.updated_at = now;
+    if(!this.created_at) {
+        this.created_at = now
+    }
+    next();
+});
+
+
