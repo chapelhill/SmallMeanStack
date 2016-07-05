aa0d5ca5c04df1708f3b12f48eb7cb7608dac950
Ervin Wu <ecwu@live.unc.edu>
2016-07-05 13:27:49 -0400

    changes

--- server.js
+++ server.js
@@ -9,30 +9,6 @@ var port= 3000;
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-// Routes
-// ------------------------------------------------------
-<<<<<<< HEAD
-   require('./app/routes.js')(app);
-=======
-app.get('/user', function (req, res) {
-  console.log('I received a GET request');
-
-  db.user.find(function (err, docs) {
-    console.log(docs);
-    res.json(docs);
-  });
-});
-
-app.post('/user', function (req, res) {
-  console.log(req.body);
-  db.user.insert(req.body, function(err, doc) {
->>>>>>> origin/master
-
-    res.json(doc);
-  });
-});
-// Listen
-// -------------------------------------------------------
 app.listen(port);
 console.log('App listening on port ' + port);
 
